4b7655cdfc8ce1bbcb8f15a5c38cc6106420bbff
Semih <semihaslan305@gmail.com>
2026-02-14 00:35:57 +0300

    Initial Commit

---
+++ appjail/appjailApp.swift
@@ -0,0 +1,17 @@
+//
+//  appjailApp.swift
+//  appjail
+//
+//  Created by Semih Aslan on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct appjailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appjailTests/appjailTests.swift
@@ -0,0 +1,17 @@
+//
+//  appjailTests.swift
+//  appjailTests
+//
+//  Created by Semih Aslan on 14.02.2026.
+//
+
+import Testing
+@testable import appjail
+
+struct appjailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
